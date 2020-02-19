f4af8add2902f8dfca251f57f16440eb993140f9
tinect <s.koenig@tinect.de>
2020-02-19 16:58:17 +0100

    create namespace in js

--- Resources/frontend/js/password-shower.js
+++ Resources/frontend/js/password-shower.js
@@ -1,62 +1,64 @@
 window.addEventListener("load", function () {
     const fields = document.querySelectorAll('input[type="password"]');
-
     if (fields.length) {
         [].forEach.call(fields, function (field) {
-            addPasswordShower(field);
+            passwordShower.init(field);
         });
     }
 });
 
-function addPasswordShower(el) {
-    if (el.classList.contains('has--password-shower')) {
-        return;
-    }
+const passwordShower = {
+    init: function (el) {
+        if (el.classList.contains('has--password-shower')) {
+            return;
+        }
+        const me = this;
 
-    el.classList.add('has--password-shower');
+        el.classList.add('has--password-shower');
 
-    const passwordShower = document.createElement('div');
-    passwordShower.classList.add('password-shower', 'is--hidden');
-    passwordShower.textContent = showPasswordTexts.show;
-
-    passwordShower.addEventListener("click", function () {
-        changePasswordType(el, passwordShower);
-    });
-
-    insertAfter(passwordShower, el);
-
-    togglePasswordShower(passwordShower, el);
-
-    el.addEventListener("keyup", function () {
-        togglePasswordShower(passwordShower, el);
-    });
-}
-
-function togglePasswordShower(passwordShower, passwordField) {
-    if (passwordField.value !== '') {
-        passwordShower.classList.remove('is--hidden');
-    } else {
-        passwordShower.classList.add('is--hidden');
-    }
-}
-
-function changePasswordType(el, passwordShower) {
-    const currentType = el.getAttribute('type');
-    if (currentType === 'password') {
-        el.setAttribute('type', 'text');
-        passwordShower.textContent = showPasswordTexts.hide;
-    } else {
-        el.setAttribute('type', 'password');
+        const passwordShower = document.createElement('div');
+        passwordShower.classList.add('password-shower', 'is--hidden');
         passwordShower.textContent = showPasswordTexts.show;
-    }
-}
 
-/* https://stackoverflow.com/a/50066247 */
-function insertAfter(newElement, targetElement) {
-    const parent = targetElement.parentNode;
-    if (parent.lastChild === targetElement) {
-        parent.appendChild(newElement);
-    } else {
-        parent.insertBefore(newElement, targetElement.nextSibling);
+        passwordShower.addEventListener("click", function () {
+            me.changePasswordType(el, passwordShower);
+        });
+
+        me.insertAfter(passwordShower, el);
+
+        me.togglePasswordShower(passwordShower, el);
+
+        el.addEventListener("keyup", function () {
+            me.togglePasswordShower(passwordShower, el);
+        });
+    },
+    togglePasswordShower: function (passwordShower, passwordField) {
+        if (passwordField.value !== '') {
+            passwordShower.classList.remove('is--hidden');
+        } else {
+            passwordShower.classList.add('is--hidden');
+        }
+    },
+
+    changePasswordType: function (el, passwordShower) {
+        const currentType = el.getAttribute('type');
+        if (currentType === 'password') {
+            el.setAttribute('type', 'text');
+            passwordShower.textContent = showPasswordTexts.hide;
+        } else {
+            el.setAttribute('type', 'password');
+            passwordShower.textContent = showPasswordTexts.show;
+        }
+    },
+
+    insertAfter: function (newElement, targetElement) {
+        /* https://stackoverflow.com/a/50066247 */
+        const parent = targetElement.parentNode;
+        if (parent.lastChild === targetElement) {
+            parent.appendChild(newElement);
+        } else {
+            parent.insertBefore(newElement, targetElement.nextSibling);
+        }
     }
-}
+};
+
